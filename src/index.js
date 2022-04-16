1a8f642d1fcbc6024a30d196baad0c9b6389dc8e
G4LH4 <totogalha@gmail.com>
2022-04-16 16:08:57 -0300

    Delete the info section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
-const readline = require("readline");
-
 const get_wifi_password = require("./Get-wifi-password/get_wifi_password.js");
-const get_wifi_info = require("./Get-wifi-info/get_wifi_info.js");
 const name = require("./name.js");
 
 (async () => {
