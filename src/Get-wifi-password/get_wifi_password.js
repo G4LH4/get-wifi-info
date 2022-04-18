741de41170989312876bd7651bd13354bdf5cf3d
G4LH4 <totogalha@gmail.com>
2022-04-18 19:31:37 -0300

    Remove save file temporally

--- src/Get-wifi-password/get_wifi_password.js
+++ src/Get-wifi-password/get_wifi_password.js
@@ -1,13 +1,10 @@
 const info_stdout = require("./Get-Info/info_stdout.js");
 const get_password = require("./Get-Info/get_password.js");
 
-const create_folder = require("./Get-Info/create_folder.js");
-
 const get_wifi_password = async (OSystem, callback) => {
   const get_info_stdout = await info_stdout(OSystem);
 
   const data = await get_password(get_info_stdout, OSystem);
-  create_folder(get_info_stdout, data);
 
   callback(data);
 
