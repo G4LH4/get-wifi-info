3fccc67a2d86f96ed89a6fda771811becd3765ed
G4LH4 <totogalha@gmail.com>
2022-04-18 22:19:09 -0300

    Add error handeling

--- src/Get-wifi-password/get_wifi_password.js
+++ src/Get-wifi-password/get_wifi_password.js
@@ -2,13 +2,15 @@ const info_stdout = require("./Get-Info/info_stdout.js");
 const get_password = require("./Get-Info/get_password.js");
 
 const get_wifi_password = async (OSystem, callback) => {
-  const get_info_stdout = await info_stdout(OSystem);
+  const get_info_stdout = await info_stdout(OSystem).catch(() => {
+    console.log("Error getting the SSID");
+  });
 
-  const data = await get_password(get_info_stdout, OSystem);
+  const data = await get_password(get_info_stdout, OSystem).catch(() => {
+    console.log("Error getting the password");
+  });
 
   callback(data);
-
-  process.exit();
 };
 
 module.exports = get_wifi_password;
