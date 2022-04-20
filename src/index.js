3d3b5d12140c3cc3fc456a88dea9b51dc5758a0d
G4LH4 <totogalha@gmail.com>
2022-04-20 17:04:46 -0300

    Delete code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
 module.exports = require("./Get-wifi-password/get_wifi_password.js");
-const get_wifi_password = require("./Get-wifi-password/get_wifi_password.js");
-
-(() => {
-  get_wifi_password("windows", async (password) => {
-    console.log(password);
-  });
-})();
