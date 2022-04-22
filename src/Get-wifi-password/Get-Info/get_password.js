6942e0449497155be268f665e3c6d1d3c6ea89ed
G4LH4 <totogalha@gmail.com>
2022-04-22 17:02:41 -0300

    add logs to try errors

--- src/Get-wifi-password/Get-Info/get_password.js
+++ src/Get-wifi-password/Get-Info/get_password.js
@@ -19,6 +19,7 @@ const OS = {
           console.error(error);
         }
 
+        console.log(stdout);
         callback(
           stdout.match(/^\s*Contenido de la clave\s+:\s*(\w+)\s*$/m)?.[1]
         );
